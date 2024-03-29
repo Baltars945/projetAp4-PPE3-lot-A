df7d21943774454131f996cd2d0a03633f4a5d39
Yann_Laurent <114476848+Baltars945@users.noreply.github.com>
2024-03-29 10:13:42 +0100

    premier exo

--- src/Controller/StockController.php
+++ src/Controller/StockController.php
@@ -10,17 +10,22 @@ use App\Repository\LIEUDISPONIBILITERepository;
 use App\Repository\MAGASINRepository;
 use App\Repository\LIEUENTREPOTRepository;
 use App\Repository\ENTREPOTRepository;
+use App\Repository\PRODUITRepository;
 
 class StockController extends AbstractController
 {
     #[Route('/stock/{id}', name: 'app_stock')]
-    public function index(int $id,Request $Request,LIEUDISPONIBILITERepository $lieudisponibiliterepository,MAGASINRepository $magasinrepository,LIEUENTREPOTRepository $lieuentrepotrepository,ENTREPOTRepository $entrepotrepository): Response
+    public function index(int $id,Request $Request,LIEUDISPONIBILITERepository $lieudisponibiliterepository
+    ,MAGASINRepository $magasinrepository,LIEUENTREPOTRepository $lieuentrepotrepository
+    ,ENTREPOTRepository $entrepotrepository, PRODUITRepository $produitrepository): Response
     {
         $lieudisponibilite = $lieudisponibiliterepository ->findAll();
         $magasin = $magasinrepository -> findAll();
         $lieuentrepot = $lieuentrepotrepository -> findAll();
         $entrepot = $entrepotrepository -> findAll();
+        $produit = $produitrepository ->findAll();
 
+        $TotalStock = 0;
         $CurrentUrl = $Request->getSchemeAndHttpHost() . $Request->getRequestUri();
         return $this->render('stock/index.html.twig', [
             'controller_name' => 'StockController',
@@ -29,6 +34,8 @@ class StockController extends AbstractController
             'magasin' => $magasin,
             'lieustockage' => $lieuentrepot,
             'entrepot' => $entrepot,
+            'TotalStock' => $TotalStock,
+            'produit' => $produit,
         ]);
     }
 }
